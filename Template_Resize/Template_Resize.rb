12fca4dec791ae0e8351f870b8ba2bf779b8f702
M.Kousaka <helloproject.odoru10th+github@gmail.com>
2019-03-18 04:27:48 +0900

    checking crop area math

--- Template_Resize/Template_Resize.rb
+++ Template_Resize/Template_Resize.rb
@@ -39,7 +39,7 @@ class Template_Resize
       @Crop_Y = ((@Y_Size.to_i + 1) * 32)
     else
       puts "Y < X"
-      @Crop_Y = (@Y_Size.to_i % 2).to_i == 0 ? (@Y_Size.to_i * 64) + 16 : (@X_Size.to_i * 32) + 16
+      @Crop_Y = (@Y_Size.to_i % 2).to_i == 0 ? ((@Y_Size.to_i + 1) * 32) + 16 : (@X_Size.to_i * 32) + 16
     end
     puts "----Crop---"
     puts "X_Pos:#{@Crop_X.to_i}"
