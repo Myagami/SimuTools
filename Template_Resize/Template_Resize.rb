f361059d7c3e7e94b837abd67f81fc861acfcfab
M.Kousaka <helloproject.odoru10th+github@gmail.com>
2019-03-18 21:48:19 +0900

    bug resize size

--- Template_Resize/Template_Resize.rb
+++ Template_Resize/Template_Resize.rb
@@ -32,14 +32,16 @@ class Template_Resize
     
     #math crop area
     @Crop_X = (@X_Size.to_i - 1) * 32
-    if @Y_Size.to_i > @X_Size.to_i then
+    if @Y_Size.to_i == 1 then
+      @Crop_Y = (@X_Size.to_i * 32) + 16
+    elsif @Y_Size.to_i > @X_Size.to_i then
       puts "Y > X"
       @Crop_Y = (@Y_Size.to_i % 2).to_i == 0 ? @Y_Size.to_i * 32 : ((@Y_Size.to_i - 1) * 32) + 16
     elsif @Y_Size.to_i == @X_Size.to_i then
       @Crop_Y = ((@Y_Size.to_i + 1) * 32)
     else
       puts "Y < X"
-      @Crop_Y = (@Y_Size.to_i % 2).to_i == 0 ? ((@Y_Size.to_i + 1) * 32) + 16 : (@X_Size.to_i * 32) + 16
+      @Crop_Y = (@Y_Size.to_i % 2).to_i == 0 ? ((@Y_Size.to_i + 1) * 32) + 32 : (@Y_Size.to_i * 32) + 16
     end
     puts "----Crop---"
     puts "X_Pos:#{@Crop_X.to_i}"
