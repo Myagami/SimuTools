c6835badddb28597ec1c4177d6f4e6bfb503f7ce
M.Kousaka <helloproject.odoru10th@gmail.com>
2019-09-30 23:08:13 +0900

    remake make rum method

--- Automake_Tools_rb/Automake_Tools.rb
+++ Automake_Tools_rb/Automake_Tools.rb
@@ -60,65 +60,87 @@ class AutoMake_Tools
     end
 
     def Make_Run(file)
+      png = file.sub(/.dat/,'') 
+
       #file type check
-        if file !~ /_src.png/ and file =~ /.png/ then # png match
-          png = file
-          dat = png.sub(/png/,'dat')
-          if !(File.exist?(dat)) then #pair dat not found return 
-            return  
-          end
-          #json check
-          jf = file.to_s.split("/")
-            jf.shift
-            puts "---------"
-            puts jf
-            puts "---------"
-            puts "png:#{file}"
-            puts "dat:Ok"
-            if @@w_Worker.has_key?(jf[0]) and @@w_Worker[jf[0]].has_key?(jf[1])  then
-              puts "Dir:"+jf[0]
-              puts "File:"+jf[1]
-              puts "X:" + @@w_Worker[jf[0]][jf[1]]['X'].to_s + " Y:" + @@w_Worker[jf[0]][jf[1]]["Y"].to_s
-                 c_Cuts = Cui_Cuts.new(file.to_s)
-                 c_Cuts.XY_Pos(@@w_Worker[jf[0]][jf[1]]["X"].to_i,@@w_Worker[jf[0]][jf[1]]["Y"].to_i)
-                 c_Cuts.Image_Prpos
-                 c_Cuts.Image_Cuts
-                 c_Cuts.Image_Write
-            else
-              puts "Target Not found"
-            end
-        elsif file !~ /_src.png/ and file =~ /.dat/ then # dat match
-          dat = file
-          if !(File.exist?(dat.sub(/\.dat/,'_src.png'))) then #exist src file
-            puts "Files"
-          else
-            if !(File.exist?(dat.sub(/dat/,'png'))) then #exist base png file
-              puts dat.sub(/dat/,'png')
-              puts "Pair file not found for single"
-              return  
-            end
-          end
-          puts "dat:#{file}"
-          puts "png:ok"
-        elsif file =~ /Worker.json/ then
-          self.Worker_Loading
+      if file =~ /goods/ and file =~ /dat/ then # goods dat
+        puts "goods"
+
+      elsif file =~ /.dat/ then # dat file
+        puts "dat"
+        puts file.to_s
+
+        #png check
+        if File.exist?(png + "_src.png") then
+          puts "exist src file"
         else
-          return
+          puts "none"
         end
 
-        #mode switch
-        if @@w_Mode == "Router" then
-          #cmd = "#{MakeObj} pak #{@@e_Dir} #{dat}"
-          cmd = "#{MakeObj} pak #{@@e_Dir} #{dat}"
-        elsif @@w_Mode == "Stands"
-          puts file
-          e_Dir = File::dirname(dat)+Pak
-          puts "Export:"+e_Dir
-          #cmd = "#{MakeObj} pak #{e_Dir} #{dat}"
-          cmd = "#{MakeObj} pak #{@@e_Dir} #{dat}"
-        end
-        puts system(cmd)
-        #puts cmd
+      elsif file =~ /.png/ then # png file
+        puts "png"
+        puts file.to_s
+      end 
+      
+      # #file type check
+      #   if file !~ /_src.png/ and file =~ /.png/ then # png match
+      #     png = file
+      #     dat = png.sub(/png/,'dat')
+      #     if !(File.exist?(dat)) then #pair dat not found return 
+      #       return  
+      #     end
+      #     #json check
+      #     jf = file.to_s.split("/")
+      #       jf.shift  
+      #       puts "---------"
+      #       puts jf
+      #       puts "---------"
+      #       puts "png:#{file}"
+      #       puts "dat:Ok"
+      #       if @@w_Worker.has_key?(jf[0]) and @@w_Worker[jf[0]].has_key?(jf[1])  then
+      #         puts "Dir:"+jf[0]
+      #         puts "File:"+jf[1]
+      #         puts "X:" + @@w_Worker[jf[0]][jf[1]]['X'].to_s + " Y:" + @@w_Worker[jf[0]][jf[1]]["Y"].to_s
+      #            c_Cuts = Cui_Cuts.new(file.to_s)
+      #            c_Cuts.XY_Pos(@@w_Worker[jf[0]][jf[1]]["X"].to_i,@@w_Worker[jf[0]][jf[1]]["Y"].to_i)
+      #            c_Cuts.Image_Prpos
+      #            c_Cuts.Image_Cuts
+      #            c_Cuts.Image_Write
+      #       else
+      #         puts "Target Not found"
+      #       end
+      #   elsif file !~ /_src.png/ and file =~ /.dat/ then # dat match
+      #     dat = file
+      #     if !(File.exist?(dat.sub(/\.dat/,'_src.png'))) then #exist src file
+      #       puts "Files"
+      #     else
+      #       if !(File.exist?(dat.sub(/dat/,'png'))) then #exist base png file
+      #         puts dat.sub(/dat/,'png')
+      #         puts "Pair file not found for single"
+      #         return
+      #       end
+      #     end
+      #     puts "dat:#{file}"
+      #     puts "png:ok"
+      #   elsif file =~ /Worker.json/ then
+      #     self.Worker_Loading
+      #   else
+      #     return
+      #   end
+
+      #   #mode switch
+      #   if @@w_Mode == "Router" then
+      #     #cmd = "#{MakeObj} pak #{@@e_Dir} #{dat}"
+      #     cmd = "#{MakeObj} pak #{@@e_Dir} #{dat}"
+      #   elsif @@w_Mode == "Stands"
+      #     puts file
+      #     e_Dir = File::dirname(dat)+Pak
+      #     puts "Export:"+e_Dir
+      #     #cmd = "#{MakeObj} pak #{e_Dir} #{dat}"
+      #     cmd = "#{MakeObj} pak #{@@e_Dir} #{dat}"
+      #   end
+      #   puts system(cmd)
+      #   #puts cmd
     end
 
     def Mode_Selects(opts)
