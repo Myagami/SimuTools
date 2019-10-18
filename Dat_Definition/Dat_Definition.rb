2b778f812cac9ee4c8eb7dec34f6a9803b13846e
M.Kousaka <helloproject.odoru10th@gmail.com>
2019-10-18 16:41:30 +0900

    png to dat

---
+++ Dat_Definition/Dat_Definition.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+#
+
+require 'optparse'
+
+class Dat_Definition
+
+  def initialize # 初期化
+    puts "Import PNG Props Export dat File"
+  end
+  
+end
+
+ExtDat = Dat_Definition.new
