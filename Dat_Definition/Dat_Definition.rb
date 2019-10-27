fe4f44be259c24fa7e69344f6722c9c26decc92e
M.Kousaka <helloproject.odoru10th@gmail.com>
2019-10-28 00:50:55 +0900

    fiX / update

--- Dat_Definition/Dat_Definition.rb
+++ Dat_Definition/Dat_Definition.rb
@@ -13,4 +13,31 @@ class Dat_Definition
   
 end
 
+#option parse
+option = {}
+OptionParser.new do |opt|
+    opt.on('-x value','role') {|val|
+        @x_size = val
+    }
+
+    opt.on('-y value','content'){|val|
+        @y_size = val
+    }
+
+    opt.on('-p value','name') {|val|
+        @png_name = val
+    }
+
+    opt.on('-w value','workformat'){|val|
+      @work_format = val
+    }
+    
+    opt.parse!(ARGV)
+end
+
+
+puts "X:#{@x_size}"
+puts "Y:#{@y_size}"
+puts "PNG:#{@png_name}"
+puts "WORKS:#{@work_format}"
 ExtDat = Dat_Definition.new
