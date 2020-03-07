840ee9805a432fe89b0b558403dd2642e1aa7749
M.Kousaka <helloproject.odoru10th@gmail.com>
2020-03-07 12:15:53 +0900

    update

--- Automake_Tools_rb/Automake_Tools.rb
+++ Automake_Tools_rb/Automake_Tools.rb
@@ -1,290 +1,282 @@
 #!/usr/bin/env ruby
-require "bundler/setup"
+# frozen_string_literal: true
+
+require 'bundler/setup'
 require 'optparse'
 require 'rb-inotify'
 require 'json'
-#require_relative 'CUI_Cuts'
-
-#my error class
+# require_relative 'CUI_Cuts'
+# my error class
 
 class AutoMake_Tools
-    Pak = '/pak/'
-    MakeObj = 'makeobj_60-2_x64'
-    Src_Prefix = '_src'    
+  Pak = '/pak/'
+  MakeObj = 'makeobj_60-2_x64'
+  Src_Prefix = '_src'
+  @@w_Dir
+  @@w_Mode
+  @@e_Dir
+  @@c_List = []
+  @@w_Worker
+  @@c_Cuts
+  @@on_sys = 'null'
+  @@action_log = {}
+  def initialize
+    # default run system
+    @@on_sys = 'Linux'
+
+    option = {}
+    OptionParser.new do |opt|
+      opt.on('-d', 'dir') do
+        # @@w_Dir = path
+        # Dir.mkdir('pak',0775)
+        puts Dir.getwd
+        puts 'WorkSpace Create'
+        exit
+      end
+
+      opt.on('-r value', 'role') do |path|
+        @@w_Dir = path
+        @@e_Dir = path + Pak
+        @@e_Dir.gsub!('//', '/')
+        @@w_Mode = 'Router'
+        puts path
+        puts 'Router'
+      end
+
+      opt.on('-s value', 'standalone') do |path|
+        @@w_Dir = path
+        @@e_Dir = path
+        @@w_Mode = 'Stands'
+
+        puts path
+        puts 'standalone'
+      end
+
+      opt.on('-m value', 'makefile') do |_v|
+        @@w_Mode = 'MakeFile'
+        puts 'makefile'
+      end
+
+      opt.on('--WSL', 'Workbase in WSL') do |_v|
+        @@on_sys = 'WSL'
+        puts 'WSL'
+      end
+      opt.parse!(ARGV)
+    end
+    self.Worker_Loading
+  end
+
+  def run_sys
+    @@on_sys
+  end
+
+  def Worker_Loading
+    worker_f = @@w_Dir + 'Worker.json'
+    if File.exist?(worker_f) # pair dat not found return
+      File.open(worker_f) do |worker|
+        @@w_Worker = JSON.load(worker)
+      end
+      puts @@w_Worker
+      puts 'Loading...'
+      nil
+    end
+  end
+
+  def Get_Working_Dir
     @@w_Dir
-    @@w_Mode
-    @@e_Dir
-    @@c_List = Array.new()
-    @@w_Worker
-    @@c_Cuts
-    @@on_sys = 'null'
-    @@action_log = {}
-    def initialize
-      #default run system
-      @@on_sys = 'Linux'
+  end
 
-      option = {}
-      OptionParser.new do |opt|
-        opt.on('-d','dir'){
-          #@@w_Dir = path
-          #Dir.mkdir('pak',0775)
-          puts Dir.getwd
-          puts "WorkSpace Create"
-          exit
-        }
+  def Make_Run(file, flugs)
+    ft = File.extname(file).to_s
+    jf = file.to_s.split('/')
+    # puts "Flag:" + flugs[0].to_s
 
-        opt.on('-r value','role') {|path|
-          @@w_Dir = path
-          @@e_Dir = path + Pak
-          @@e_Dir.gsub!("//","/")
-          @@w_Mode = 'Router'
-          puts path
-          puts "Router"
-        }
-
-        opt.on('-s value','standalone'){|path|
-          @@w_Dir = path
-          @@e_Dir = path
-          @@w_Mode = 'Stands'
-          
-          puts path
-          puts "standalone"
-        }
-
-        opt.on('-m value','makefile') {|v|
-          @@w_Mode = 'MakeFile'  
-          puts "makefile"
-        }
-
-        opt.on('--WSL','Workbase in WSL'){|v| 
-          @@on_sys = 'WSL'         
-          puts "WSL"
-        }
-        opt.parse!(ARGV)
-      end
-      self.Worker_Loading
-      
+    # file delete
+    if flugs[0].to_s == 'delete' && file =~ /locking/
+      self.Logging_Importer('LockDirectory', jf[1])
+      self.Logging_Importer('status', 'Unlock')
+      self.Logging_Exporter()
+      return 0
+    elsif flugs[0].to_s == 'delete' && file =~ /git/
+      return 0 
+    elsif flugs[0].to_s == 'delete'
+      self.Logging_Importer('status', 'delete')
+      self.Logging_Importer('FilePath', file.to_s)
+      self.Logging_Exporter
     end
 
-    def run_sys
-      return @@on_sys
-    end
-
-    def Worker_Loading
-      worker_f = @@w_Dir + "Worker.json"
-      if (File.exist?(worker_f)) then #pair dat not found return 
-        File.open(worker_f){|worker|
-          @@w_Worker = JSON.load(worker)
-        }
-        puts @@w_Worker
-        puts "Loading..."
-        return  
-      end
-
-    end
-    
-    def Get_Working_Dir
-        return @@w_Dir
-    end
-
-    def Make_Run(file,flugs)
-      ft = File.extname(file).to_s
-      jf = file.to_s.split("/")
-      #puts "Flag:" + flugs[0].to_s
-
-      #file delete
-      if flugs[0].to_s == "delete" && file =~ /locking/ then 
-        self.Logging_Importer("LockDirectory",jf[1])
-        self.Logging_Importer("status","Unlock")
-        self.Logging_Exporter() 
-        return 0
-      elsif flugs[0].to_s == "delete"
-          
-        self.Logging_Importer("status","delete")
-        self.Logging_Importer("FilePath",file.to_s)
+    # File Type Checking
+    if file =~ /goods/ && (ft == '.dat') # goods dat
+      # puts "goods"
+      self.Logging_Importer('status', 'update')
+      self.Logging_Importer('FileType', 'goods')
+      self.Logging_Importer('FilePath', file.to_s)
+      self.Command_Genelate(file)
+    elsif ft == '.dat' # dat file
+      # locking file exist check
+      self.Logging_Importer('status', 'update')
+      self.Logging_Importer('FileType', 'dat')
+      self.Logging_Importer('FilePath', file.to_s)
+      if Locking_File_Check(jf[0], jf[1]) == 0
+        self.Logging_Importer('Locking', 'true')
         self.Logging_Exporter
         return 0
       end
-
-      #File Type Checking      
-      if file =~ /goods/ and ft == ".dat" then # goods dat
-        #puts "goods"
-        self.Logging_Importer("status","update")
-        self.Logging_Importer("FileType","goods")
-        self.Logging_Importer("FilePath",file.to_s)
-        self.Command_Genelate(file)
-      elsif ft == ".dat" then # dat file
-        #locking file exist check
-        self.Logging_Importer("status","update")
-        self.Logging_Importer("FileType","dat")
-        self.Logging_Importer("FilePath",file.to_s)
-        if Locking_File_Check(jf[0],jf[1]) == 0
-          self.Logging_Importer("Locking","true")
-          self.Logging_Exporter
-          return 0
-        end
-        #puts "dat"
-        fn = file.gsub(/\.dat/,'')
-        #png check
-        if File.exist?(fn + "_S_src.png".to_s) then # cut and cur
-          self.Logging_Importer("FileExist","Cur src file")
-          puts "exist cur src file"
-        elsif File.exist?(fn + "_S.png".to_s) then # cur only
-          self.Logging_Importer("FileExist","Cur file")
-        elsif File.exist?(fn + "_src.png".to_s) then # cut only
-          self.Logging_Importer("FileExist","src file")
-        elsif File.exist?(fn + ".png".to_s) then # cut only
-          self.Logging_Importer("FileExist","single file")
-        end
-        cmd = self.Command_Genelate(file)
-      elsif ft == ".png" then # png file
-        #split dir path for json loading
-        self.Logging_Importer("status","update")
-        self.Logging_Importer("FileType","dat")
-        self.Logging_Importer("FilePath",file.to_s)
-        if Locking_File_Check(jf[0],jf[1]) == 0
-          self.Logging_Importer("Locking","true")
-          self.Logging_Exporter
-          return 0
-        end
-        #worker job data getting]
-        if defined? @@w_Worker then
-          if @@w_Worker.has_key?(jf[1]) and @@w_Worker[jf[1]].has_key?(jf[2]) and @@w_Worker[jf[1]][jf[2]].has_key?(jf[3].gsub!(/\.png/,'')) then # exist job
-            # [0] = Working Directory
-            # [1] = Sub Working Directory
-            puts "Job true"
-            self.Logging_Importer("Working",file.to_sjf[1],"Worker")
-            self.Logging_Importer("Subwork",file.to_sjf[2],"Worker")
-            self.Logging_Importer("Target",file.to_sjf[3],"Worker")
-            puts "Working:"+jf[1]
-            puts "SubWork:"+jf[2]
-            puts "Target:"+jf[3]  
-            puts @@w_Worker[jf[1]][jf[2]][jf[3]]
-            wj = @@w_Worker[jf[1]][jf[2]][jf[3]]
-            # image cut
-            system("CUI_Cuts.rb #{file.to_s} #{wj["X"]} #{wj["Y"]}")
-          else
-            print "span"
-            puts @@w_Worker
-          end
-        else
-          self.Logging_Importer("Working","Single")
-          puts "Single User" 
-        end
-
-
-        #path convert
-        if file.to_s =~ /.*_(S|N|E|W)\./ then # cur
-          puts "cur"
-          dat =  file.gsub(/(?<path>.*)_(S|N|E|W).png/,'\k<path>.dat')
-        elsif file.to_s =~ /.*_src/ then # src
-          puts "src"
-          dat = file.gsub(/(?<path>.*)_src.png/,'\k<path>.dat')
-        else # single
-          puts "single"
-          dat = file.gsub(/\.png/,'.dat')
-        end
-
-        if File.exist?(dat) then 
-          cmd = self.Command_Genelate(dat)
-        else
-          puts "dat not found"
-          return
-        end 
-      elsif file =~ /locking/ then
-        self.Logging_Importer("LockDirectory",jf[1])
-        self.Logging_Importer("status","Locking")
-        self.Logging_Exporter() 
-      elsif file =~ /Worker.json/ then #reload Worker.json
-        self.Worker_Loading
-      end 
-      # export system command
-      if cmd.nil? == false then
-        system("#{cmd}")
+      # puts "dat"
+      fn = file.gsub(/\.dat/, '')
+      # png check
+      if File.exist?(fn + '_S_src.png'.to_s) # cut and cur
+        self.Logging_Importer('FileExist', 'Cur src file')
+        puts 'exist cur src file'
+      elsif File.exist?(fn + '_S.png'.to_s) # cur only
+        self.Logging_Importer('FileExist', 'Cur file')
+      elsif File.exist?(fn + '_src.png'.to_s) # cut only
+        self.Logging_Importer('FileExist', 'src file')
+      elsif File.exist?(fn + '.png'.to_s) # cut only
+        self.Logging_Importer('FileExist', 'single file')
+      end
+      cmd = self.Command_Genelate(file)
+    elsif ft == '.png' # png file
+      # split dir path for json loading
+      self.Logging_Importer('status', 'update')
+      self.Logging_Importer('FileType', 'dat')
+      self.Logging_Importer('FilePath', file.to_s)
+      if Locking_File_Check(jf[0], jf[1]) == 0
+        self.Logging_Importer('Locking', 'true')
         self.Logging_Exporter
-        puts "-----------------"
-      end
-
-    end
-
-    def Command_Genelate(file,path=@@e_Dir)
-      puts "Export command"
-      if @@w_Mode == "Router" then
-        #cmd = "#{MakeObj} pak #{@@e_Dir} #{dat}"
-        cmd = "#{MakeObj} pak #{path} #{file}"
-        self.Logging_Importer("Mode","Router","Export")
-        self.Logging_Importer("Command","#{cmd.to_s}","Export")
-        #puts "Router"
-        #cmd = "#{MakeObj} pak #{path} #{file}"
-      elsif @@w_Mode == "Stands"
-        #based = File.dirname(path)
-        cmd = "#{MakeObj} pak #{path}/pak/ #{file}"
-      end
-      return cmd
-    end
-
-    def Mode_Selects(opts)
-        t_RunMode = {r:"Router",s:"Stands",m:"makefile"}
-        @@w_Mode = t_RunMode[opts.to_sym]
-        
-    end
-    
-    def Tool_Propertys
-        puts "------Tool Status------"
-        puts "System:"+@@on_sys
-        puts "Working Directory:"+@@w_Dir
-        puts "Export Directory:"+@@e_Dir
-        puts "Mode:"+ @@w_Mode.to_s
-        puts "----------------------"
-    end 
-
-    def Locking_File_Check(root,work)
-      if File.exist?("#{root}/#{work}/locking") then
         return 0
       end
-    end
-
-    def Logging_Importer(type,message,mother="Root")
-      if mother == "Root" then
-        @@action_log[:"#{type.to_sym}"] = message
-      elsif @@action_log.has_key?(:"#{mother.to_sym}") then
-        @@action_log[:"#{mother.to_sym}"].store("#{type.to_s}",message)
+      # worker job data getting]
+      if defined? @@w_Worker
+        if @@w_Worker.key?(jf[1]) && @@w_Worker[jf[1]].key?(jf[2]) && @@w_Worker[jf[1]][jf[2]].key?(jf[3].gsub!(/\.png/, '')) # exist job
+          # [0] = Working Directory
+          # [1] = Sub Working Directory
+          puts 'Job true'
+          self.Logging_Importer('Working', file.to_sjf[1], 'Worker')
+          self.Logging_Importer('Subwork', file.to_sjf[2], 'Worker')
+          self.Logging_Importer('Target', file.to_sjf[3], 'Worker')
+          puts 'Working:' + jf[1]
+          puts 'SubWork:' + jf[2]
+          puts 'Target:' + jf[3]
+          puts @@w_Worker[jf[1]][jf[2]][jf[3]]
+          wj = @@w_Worker[jf[1]][jf[2]][jf[3]]
+          # image cut
+          system("CUI_Cuts.rb #{file} #{wj['X']} #{wj['Y']}")
+        else
+          print 'span'
+          puts @@w_Worker
+        end
       else
-        _has = {}
-        #puts mother
-        _has[:"#{type.to_sym}"] = message
-        @@action_log[:"#{mother.to_sym}"] = _has
+        self.Logging_Importer('Working', 'Single')
+        puts 'Single User'
       end
+
+      # path convert
+      if file.to_s =~ /.*_(S|N|E|W)\./ # cur
+        puts 'cur'
+        dat = file.gsub(/(?<path>.*)_(S|N|E|W).png/, '\k<path>.dat')
+      elsif file.to_s =~ /.*_src/ # src
+        puts 'src'
+        dat = file.gsub(/(?<path>.*)_src.png/, '\k<path>.dat')
+      else # single
+        puts 'single'
+        dat = file.gsub(/\.png/, '.dat')
+      end
+
+      if File.exist?(dat)
+        cmd = self.Command_Genelate(dat)
+      else
+        puts 'dat not found'
+        return
+      end
+    elsif file =~ /locking/
+      self.Logging_Importer('LockDirectory', jf[1])
+      self.Logging_Importer('status', 'Locking')
+      self.Logging_Exporter()
+    elsif file =~ /Worker.json/ # reload Worker.json
+      self.Worker_Loading
     end
+    # export system command
+    if cmd.nil? == false
+      system(cmd.to_s)
+      self.Logging_Exporter
+      puts '-----------------'
+    end
+  end
 
-    def Logging_Exporter
-      puts JSON.pretty_generate(@@action_log)
-      @@action_log = {}
-    end 
-end 
+  def Command_Genelate(file, path = @@e_Dir)
+    puts 'Export command'
+    if @@w_Mode == 'Router'
+      # cmd = "#{MakeObj} pak #{@@e_Dir} #{dat}"
+      cmd = "#{MakeObj} pak #{path} #{file}"
+      self.Logging_Importer('Mode', 'Router', 'Export')
+      self.Logging_Importer('Command', cmd.to_s, 'Export')
+      # puts "Router"
+      # cmd = "#{MakeObj} pak #{path} #{file}"
+    elsif @@w_Mode == 'Stands'
+      # based = File.dirname(path)
+      cmd = "#{MakeObj} pak #{path}/pak/ #{file}"
+    end
+    cmd
+  end
 
-AMT = AutoMake_Tools.new()
+  def Mode_Selects(opts)
+    t_RunMode = { r: 'Router', s: 'Stands', m: 'makefile' }
+    @@w_Mode = t_RunMode[opts.to_sym]
+  end
+
+  def Tool_Propertys
+    puts '------Tool Status------'
+    puts 'System:' + @@on_sys
+    puts 'Working Directory:' + @@w_Dir
+    puts 'Export Directory:' + @@e_Dir
+    puts 'Mode:' + @@w_Mode.to_s
+    puts '----------------------'
+  end
+
+  def Locking_File_Check(root, work)
+    return 0 if File.exist?("#{root}/#{work}/locking")
+  end
+
+  def Logging_Importer(type, message, mother = 'Root')
+    if mother == 'Root'
+      @@action_log[:"#{type.to_sym}"] = message
+    elsif @@action_log.key?(:"#{mother.to_sym}")
+      @@action_log[:"#{mother.to_sym}"].store(type.to_s, message)
+    else
+      _has = {}
+      # puts mother
+      _has[:"#{type.to_sym}"] = message
+      @@action_log[:"#{mother.to_sym}"] = _has
+    end
+  end
+
+  def Logging_Exporter
+    puts JSON.pretty_generate(@@action_log)
+    @@action_log = {}
+  end
+end
+
+AMT = AutoMake_Tools.new
 AMT.Tool_Propertys
-#AMT.Path_Monitor
-sys =  AMT.run_sys
+# AMT.Path_Monitor
+sys = AMT.run_sys
 
 notif = INotify::Notifier.new
 
-if sys == 'WSL' then
-  notif.watch(AMT.Get_Working_Dir,:close_write,:recursive,:attrib,:remove){
-    |fev|
-    #puts fev
+if sys == 'WSL'
+  notif.watch(AMT.Get_Working_Dir, :close_write, :recursive, :attrib, :remove) do |fev|
+    # puts fev
     file = fev.absolute_name
-    AMT.Make_Run(file,fev.flags)
-    #puts "#{@@w_Dir} / #{fev.flags} / #{fev.absolute_name}"
-  }
-elsif sys == 'Linux' then
-  notif.watch(AMT.Get_Working_Dir,:close_write,:recursive,:delete){
-    |fev|
-    #puts fev.flags
+    AMT.Make_Run(file, fev.flags)
+    # puts "#{@@w_Dir} / #{fev.flags} / #{fev.absolute_name}"
+  end
+elsif sys == 'Linux'
+  notif.watch(AMT.Get_Working_Dir, :close_write, :recursive, :delete) do |fev|
+    # puts fev.flags
     file = fev.absolute_name
-    AMT.Make_Run(file,fev.flags)
-    #puts "#{@@w_Dir} / #{fev.flags} / #{fev.absolute_name}"
-  }
+    AMT.Make_Run(file, fev.flags)
+    # puts "#{@@w_Dir} / #{fev.flags} / #{fev.absolute_name}"
+  end
 end
 notif.run
--- Automake_Tools_rb/CUI_Cuts.rb
+++ Automake_Tools_rb/CUI_Cuts.rb
@@ -1,178 +1,175 @@
 #!/usr/bin/env ruby
-# coding: utf-8
+# frozen_string_literal: true
 
-#require and new instance
-require "bundler/setup"
+# require and new instance
+require 'bundler/setup'
 require 'rmagick'
 
-#crop
+# crop
 class CUI_Cuts
-  def initialize(in_image) #start
+  def initialize(in_image) # start
     @In_Image = in_image
-    @Out_Image = in_image.sub(/.png/,'_src.png')
+    @Out_Image = in_image.sub(/.png/, '_src.png')
     @image = Magick::ImageList.new(in_image)
-    puts "In:"+in_image
-    
+    puts 'In:' + in_image
   end
 
-  def XY_Pos(x_size,y_size)
+  def XY_Pos(x_size, y_size)
     @X_Size = x_size
     @Y_Size = y_size
-    @image_Crops = Array.new(@Y_Size.to_i).map{Array.new(@X_Size.to_i)}    
+    @image_Crops = Array.new(@Y_Size.to_i).map { Array.new(@X_Size.to_i) }
   end
 
   def Image_Props
-    print "Export: "
+    print 'Export: '
     puts @Out_Image
 
-    #props
-    print "Width:"
+    # props
+    print 'Width:'
     puts @image.columns
-    print "Height:"
+    print 'Height:'
     puts @image.rows
-    print "X:"
+    print 'X:'
     puts @X_Size
-    print "Y:"
+    print 'Y:'
     puts @Y_Size
 
     @Crop_X = (@X_Size.to_i - 1) * 32
-    puts "CropX:"+@Crop_X.to_s
-    if @Y_Size.to_i > @X_Size.to_i then
-      puts "Y > X"
+    puts 'CropX:' + @Crop_X.to_s
+    if @Y_Size.to_i > @X_Size.to_i
+      puts 'Y > X'
       @Crop_Y = (@Y_Size.to_i % 2).to_i == 0 ? ((@Y_Size.to_i / 2) * 32) + 16 : ((@Y_Size.to_i - 1) * 32) - 32
-    elsif @Y_Size.to_i == @X_Size.to_i then
-      @Crop_Y = (@Y_Size.to_i - 1) * 32 
+    elsif @Y_Size.to_i == @X_Size.to_i
+      @Crop_Y = (@Y_Size.to_i - 1) * 32
     else
-      puts "Y < X"
-      if @Y_Size == 1 then
+      puts 'Y < X'
+      if @Y_Size == 1
         @Crop_Y = 16
       else
-        @Crop_Y = (@Y_Size.to_i % 2).to_i == 0 ? (((@Y_Size.to_i - 1) * 32) + 16) : @Y_Size.to_i  * 32
+        @Crop_Y = (@Y_Size.to_i % 2).to_i == 0 ? (((@Y_Size.to_i - 1) * 32) + 16) : @Y_Size.to_i * 32
       end
     end
-    
+
     @Crop_Y = 16
-    puts "CropY:"+@Crop_Y.to_s
+    puts 'CropY:' + @Crop_Y.to_s
   end
 
-  def Image_Cuts #Image Cutter
+  def Image_Cuts # Image Cutter
     ct_x = 1
-    #y
-    for ct_y , y_pos in 1..@Y_Size.to_i do
+    # y
+    (1..@Y_Size.to_i).each do |ct_y, y_pos|
+      puts "ct_x:#{ct_x}"
+      puts "ct_y:#{ct_y}"
 
-      puts "ct_x:#{ct_x.to_s}" 
-      puts "ct_y:#{ct_y.to_s}"
-      
       x_pos = ct_y.to_i == 1 ? @Crop_X.to_i + ((ct_y.to_i - 1) * 16) : @Crop_X.to_i + ((ct_y.to_i - 1) * 16) + ((ct_y.to_i - 1) * 16)
-      if ct_y.to_i == 1 then
-        y_pos = @Crop_Y
-      else
-        y_pos = @Crop_Y.to_i - ((ct_y.to_i - 1) * 16)
-      end
-      
+      y_pos = if ct_y.to_i == 1
+                @Crop_Y
+              else
+                @Crop_Y.to_i - ((ct_y.to_i - 1) * 16)
+              end
+
       print "----\n"
-      #x
-      for ct_x in 1..X_Size.to_i do
-        print "X: " 
+      # x
+      (1..X_Size.to_i).each do |ct_x|
+        print 'X: '
         puts ct_x.to_i
-        print "Y: " 
+        print 'Y: '
         puts ct_y.to_i
-        
-        print "Y_pos: "
-        puts y_pos 
-        print "X_pos: "
-        puts x_pos 
 
-        #override
-        c_Image = @image.crop(Magick::SouthEastGravity,x_pos.to_i,y_pos.to_i,64,64,true){
-          self.background_color='red'
-        }
+        print 'Y_pos: '
+        puts y_pos
+        print 'X_pos: '
+        puts x_pos
 
-        #masking
-        #common
-        mask = Magick::Image.new(64,64){self.background_color='none'}
-        mask_Path = Magick::Draw.new()
+        # override
+        c_Image = @image.crop(Magick::SouthEastGravity, x_pos.to_i, y_pos.to_i, 64, 64, true) do
+          self.background_color = 'red'
+        end
+
+        # masking
+        # common
+        mask = Magick::Image.new(64, 64) { self.background_color = 'none' }
+        mask_Path = Magick::Draw.new
         mask_Path.stroke_antialias(false)
-        mask_Path.clip_rule("evenodd") 
-        mask_Path.polygon(0,49,
-                          28,63,
-                          29,63,
-                          0,63)
-        mask_Path.polygon(63,49,
-                          35,63,
-                          34,63,
-                          63,63)
-        #center
-        if ct_y <= 1 && ct_x > 1 then
+        mask_Path.clip_rule('evenodd')
+        mask_Path.polygon(0, 49,
+                          28, 63,
+                          29, 63,
+                          0, 63)
+        mask_Path.polygon(63, 49,
+                          35, 63,
+                          34, 63,
+                          63, 63)
+        # center
+        if ct_y <= 1 && ct_x > 1
           print "1st line\n"
-          mask_Path.polygon(0,0,
-                            0,47,
-                            30,32,
-                            31,32,
-                            31,0)
-        elsif ct_y > 1 && ct_x == 1 then
-          mask_Path.polygon(32,0,
-                            32,32,
-                            33,32,
-                            63,47,
-                            63,0)
-         elsif ct_y > 1 && ct_x != 1 then
-           mask_Path.polygon(0,0,
-                             0,47,
-                             30,32,
-                             31,32,
-                             31,0)
-           mask_Path.polygon(32,0,
-                             32,32,
-                             33,32,
-                             63,47,
-                             63,0)
-           print "center Y\n" 
+          mask_Path.polygon(0, 0,
+                            0, 47,
+                            30, 32,
+                            31, 32,
+                            31, 0)
+        elsif ct_y > 1 && ct_x == 1
+          mask_Path.polygon(32, 0,
+                            32, 32,
+                            33, 32,
+                            63, 47,
+                            63, 0)
+        elsif ct_y > 1 && ct_x != 1
+          mask_Path.polygon(0, 0,
+                            0, 47,
+                            30, 32,
+                            31, 32,
+                            31, 0)
+          mask_Path.polygon(32, 0,
+                            32, 32,
+                            33, 32,
+                            63, 47,
+                            63, 0)
+          print "center Y\n"
         end
         mask_Path.draw(mask)
-        
 
-        #bottom
-        @image_Crops[ct_y.to_i - 1][ct_x.to_i - 1] = mask.composite(c_Image,0,0,Magick::SrcOutCompositeOp)
-        x_pos = x_pos - 32
-        y_pos = y_pos - 16
+        # bottom
+        @image_Crops[ct_y.to_i - 1][ct_x.to_i - 1] = mask.composite(c_Image, 0, 0, Magick::SrcOutCompositeOp)
+        x_pos -= 32
+        y_pos -= 16
       end
       print "---------\n\n"
-    end    
+    end
   end
 
   def Image_Write
-    exb = Magick::Image.new(X_Size.to_i*64,Y_Size.to_i*64){
-       self.background_color="#E7FFFF"
-    }
+    exb = Magick::Image.new(X_Size.to_i * 64, Y_Size.to_i * 64) do
+      self.background_color = '#E7FFFF'
+    end
 
     ct_y = 0
     ct_x = 0
-    expo = 0 
+    expo = 0
     @image_Crops.each do |y_img|
       y_img.each do |x_img|
-        exb.composite!(x_img,ct_x * 64,ct_y * 64,Magick::OverCompositeOp)
-        ct_x = ct_x + 1
+        exb.composite!(x_img, ct_x * 64, ct_y * 64, Magick::OverCompositeOp)
+        ct_x += 1
       end
-      ct_x = 0 
-      print "\n" 
-      ct_y = ct_y + 1
+      ct_x = 0
+      print "\n"
+      ct_y += 1
     end
-    
-    #Export
+
+    # Export
     exb.write(@Out_Image)
   end
 end
 
-#Single actions
-In_Image = ARGV[0] 
-X_Size = ARGV[1] 
-Y_Size  = ARGV[2] 
+# Single actions
+In_Image = ARGV[0]
+X_Size = ARGV[1]
+Y_Size = ARGV[2]
 
 require_relative 'CUI_Cuts'
 
 cuts = CUI_Cuts.new(In_Image)
-cuts.XY_Pos(X_Size,Y_Size)
+cuts.XY_Pos(X_Size, Y_Size)
 cuts.Image_Props
 cuts.Image_Cuts
 cuts.Image_Write
