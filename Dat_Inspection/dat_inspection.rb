11290190f2c3ad2ade63f1530912f9f35144e6dd
honoka <hayate.works@gmail.com>
2021-02-04 07:59:29 +0900

    fileload

--- Dat_Inspection/dat_inspection.rb
+++ Dat_Inspection/dat_inspection.rb
@@ -1,2 +1,32 @@
 #!/usr/bin/env ruby
 
+dat_f = ARGV[0]
+obj = []
+ct = 0 
+cnf = {}
+
+puts dat_f
+
+#read datfile
+File.open(dat_f) do | dat_l |
+  dat_l.each_line do |dat_c| #object 
+    if dat_c =~ /obj/
+      cnf["obj"] = dat_c.split(/=/)[1].chomp!
+      puts cnf 
+      puts "object"
+    elsif dat_c =~ /^[-]{1,}$/ #split line
+      obj << cnf
+      puts "CT:"+ct.to_s
+      ct += 1
+      cnf = {}
+    elsif dat_c =~ /=> / #icon
+      puts dat_c
+    else #other 
+      line = dat_c.split(/=/)
+      cnf[line[0]] = line[1].chomp!
+    end
+    
+  end
+end
+
+puts obj
