3f4134dfc75d84fe04bdd627d08a46e38d3793d7
honoka <hayate.works@gmail.com>
2021-02-04 10:02:15 +0900

    inspection phase

--- Dat_Inspection/dat_inspection.rb
+++ Dat_Inspection/dat_inspection.rb
@@ -1,26 +1,32 @@
 #!/usr/bin/env ruby
 
+#debug
+require 'pp'
+
+#use vars
 dat_f = ARGV[0]
 obj = []
 ct = 0 
 cnf = {}
 
-puts dat_f
+#puts dat_f
 
-#read datfile
+#read datfile phese
 File.open(dat_f) do | dat_l |
   dat_l.each_line do |dat_c| #object 
     if dat_c =~ /obj/
       cnf["obj"] = dat_c.split(/=/)[1].chomp!
-      puts cnf 
-      puts "object"
+      #puts cnf 
+      #puts "object"
     elsif dat_c =~ /^[-]{1,}$/ #split line
       obj << cnf
-      puts "CT:"+ct.to_s
+      #puts "CT:"+ct.to_s
       ct += 1
       cnf = {}
     elsif dat_c =~ /=> / #icon
-      puts dat_c
+      line = dat_c.split(/=> /)
+      cnf[line[0]] = line[1].chomp!
+      #puts dat_c
     else #other 
       line = dat_c.split(/=/)
       cnf[line[0]] = line[1].chomp!
@@ -29,4 +35,13 @@ File.open(dat_f) do | dat_l |
   end
 end
 
-puts obj
+#pp obj
+
+#inspection phase
+obj.each{|objc| # 1 object unit each
+  objc.each{|key,val| # parse key and value
+    puts "Key:"+key+" = "+val
+  }
+  #pp objc 
+  puts "----"
+}
