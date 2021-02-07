055a6c17584d16c5c9a64e4b4c33e8c33582ac4a
M.Kousaka <hayate.works@gmail.com>
2021-02-07 14:51:00 +0900

    fix: dir / file name

--- libs/install.sh
+++ libs/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ln -sf ~/Tools/SimuTools/Dat_Inspection/dat_inspection.rb ~/Tools/SimuTools/libs
+ln -sf ~/Tools/SimuTools/DatInspection/DatInspection.rb ~/Tools/SimuTools/libs
 ln -sf ~/Tools/SimuTools/CUI_Cuts/CUI_Cuts.rb ~/Tools/SimuTools/libs
 
 
